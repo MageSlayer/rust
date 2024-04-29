454e09b87330843130578e0896388c798c377f64
MageSlayer <denis.golovan@gmail.com>
2024-04-29 18:21:45 +0300

    Script cleanups

--- build-toolchain.sh
+++ build-toolchain.sh
@@ -1,51 +1,42 @@
 #!/bin/bash
 
-#./x build --stage 2 --target x86_64-unknown-linux-gnu
-#./x build --target x86_64-pc-windows-gnu
+# Local Rust compilers build script
+
+set -e
 
 export PATH=/usr/local/osx-ndk-x86/bin:$(pwd):${PATH}
 export LD_LIBRARY_PATH=/usr/local/osx-ndk-x86/lib
 
-#ln -sf $(which x86_64-apple-darwin20.4-cc) x86_64-apple-darwin20.4-cc
-#ln -sf x86_64-apple-darwin20.4-cc cc
-
-#rm -f cc
-#cat > ./cc <<EOF
-##!/bin/bash
-#PATH=/usr/local/osx-ndk-x86/bin:\${PATH} LD_LIBRARY_PATH=/usr/local/osx-ndk-x86/lib x86_64-apple-darwin20.4-cc \$@
-#EOF
-#chmod a+x ./cc
-
-#+
-#export CC_x86_64_apple_darwin=x86_64-apple-darwin20.4-cc
-#./x build --stage 2 --target x86_64-apple-darwin
-
-#+
-#export CC_aarch64_apple_darwin=aarch64-apple-darwin20.4-cc
-#./x build --stage 2 --target aarch64-apple-darwin
-
-#export CC_x86_64_pc_windows_gnu=x86_64-w64-mingw32-gcc
-#./x build --stage 2 --target x86_64-pc-windows-gnu
-
-#export CC=o64-clang
-#export CXX=o64-clang++
-
-#export TARGET=x86_64-apple-darwin
-#export CFLAGS='-arch x86_64 -O2'
-#export CC=x86_64-apple-darwin20.4-cc
-#export CC_x86_64_apple_darwin=o64-clang
-
-#export CXX=x86_64-apple-darwin20.4-c++
-#export AR=x86_64-apple-darwin20.4-ar
-#export AS=x86_64-apple-darwin20.4-cc
-#export LD=x86_64-apple-darwin20.4-ld
-#export PKG_CONFIG_ALLOW_CROSS=1
-
 export CC_x86_64_apple_darwin=x86_64-apple-darwin20.4-cc
 export CC_aarch64_apple_darwin=aarch64-apple-darwin20.4-cc
 export CC_x86_64_pc_windows_gnu=x86_64-w64-mingw32-gcc
-./x build --stage 2 --target x86_64-unknown-linux-gnu,x86_64-apple-darwin,aarch64-apple-darwin,x86_64-pc-windows-gnu
 
+function sanitizers() {
+    # move sanitizers in place, only Linux support
+    # otherwise following error occurs
+    # note: cc: error: {$HOME}/rust/build/x86_64-unknown-linux-gnu/stage2/lib/rustlib/x86_64-unknown-linux-gnu/lib/librustc-dev_rt.lsan.a: No such file or directory
 
-#,x86_64-apple-darwin,x86_64-pc-windows-gnu
+    pushd build/x86_64-unknown-linux-gnu
+    mv native/sanitizers/build/lib/linux/*  stage2/lib/rustlib/x86_64-unknown-linux-gnu/lib/
+    popd
+}
+
+H=$(hostname)
+if [[ "$H" != "builderust.dev.ath" ]]; then
+    # non-split build
+    ./x build --stage 2 --target x86_64-unknown-linux-gnu,x86_64-apple-darwin,aarch64-apple-darwin,x86_64-pc-windows-gnu
+    sanitizers
+else
+    # split compilation
+    . /etc/os-release
+    if [[ "devuan" == "$ID" ]]; then
+	# chrooted build
+	./x build --stage 2 --target x86_64-apple-darwin,aarch64-apple-darwin,x86_64-pc-windows-gnu
+    else
+	# main build
+	./x build --stage 2 --target x86_64-unknown-linux-gnu
+	sanitizers
+    fi
+fi
+
 

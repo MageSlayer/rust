1f869f9d551890f49d6f688d4446a0b54932237a
MageSlayer <denis.golovan@gmail.com>
2024-05-01 16:46:34 +0300

    CentOS llvm-11

---
+++ llvm-11.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo bash -c 'cat << EOF > /etc/yum.repos.d/llvmtoolset-build.repo
+[llvmtoolset-build]
+name=LLVM Toolset 11.0 - Build
+baseurl=https://buildlogs.centos.org/c7-llvm-toolset-11.0.x86_64/
+gpgcheck=0
+enabled=1
+EOF'
+
+sudo yum install --nogpgcheck llvm-toolset-11.0-clang-tools-extra llvm-toolset-11.0-clang llvm-toolset-11.0-lld.x86_64
+
+# TODO
+# source /opt/rh/llvm-toolset-11.0/enable

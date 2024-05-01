4179fa1e59311ead2d0baced38cb2995ed2156f0
MageSlayer <denis.golovan@gmail.com>
2024-05-02 00:05:13 +0300

    libstdc++ install

--- llvm-11.sh
+++ llvm-11.sh
@@ -8,7 +8,7 @@ gpgcheck=0
 enabled=1
 EOF'
 
-sudo yum install --nogpgcheck llvm-toolset-11.0-clang-tools-extra llvm-toolset-11.0-clang llvm-toolset-11.0-lld.x86_64
+sudo yum install --nogpgcheck llvm-toolset-11.0-clang-tools-extra llvm-toolset-11.0-clang llvm-toolset-11.0-lld.x86_64 llvm-toolset-11.0-llvm-devel.x86_64 devtoolset-11-gcc-c++.x86_64
 
 # TODO
 # source /opt/rh/llvm-toolset-11.0/enable
